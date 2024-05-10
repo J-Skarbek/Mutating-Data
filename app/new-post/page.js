a54acddc1470866668d438bd8d34ce22a543e5ba
J-Skarbek <john@snarkmediaagency.com>
2024-05-10 13:05:56 -0500

    Flesh out user feedback from server side error handling

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,13 +1,10 @@
-'use client';
-
-import FormSubmit from '@/components/formSubmit';
+import PostForm from '@/components/postForm';
 import { storePost } from '@/lib/posts';
 import { redirect } from 'next/navigation';
-import { useFormState } from 'react/dom';
 
 export default function NewPostPage() {
   
-  async function createPost(formData) {
+  async function createPost(prevState, formData) {
     'use server';
     const title = formData.get('title');
     const image = formData.get('image');
@@ -40,33 +37,7 @@ export default function NewPostPage() {
     redirect('/feed');
   }
 
-  const [state, formAction] = useFormState(createPost, {});
-
   return (
-    <>
-      <h1>Create a new post</h1>
-      <form action={formAction}>
-        <p className="form-control">
-          <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" />
-        </p>
-        <p className="form-control">
-          <label htmlFor="image">Image URL</label>
-          <input
-            type="file"
-            accept="image/png, image/jpeg"
-            id="image"
-            name="image"
-          />
-        </p>
-        <p className="form-control">
-          <label htmlFor="content">Content</label>
-          <textarea id="content" name="content" rows="5" />
-        </p>
-        <p className="form-actions">
-          <FormSubmit />
-        </p>
-      </form>
-    </>
-  );
+    <PostForm action={createPost}/>
+  )
 }
