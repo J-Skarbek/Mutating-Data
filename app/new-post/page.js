e332dc681ac6a14d6747b2d40ee02fce1315f3d9
J-Skarbek <john@snarkmediaagency.com>
2024-05-10 13:11:57 -0500

    Refactor creatPost to its own server action file

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,42 +1,7 @@
 import PostForm from '@/components/postForm';
-import { storePost } from '@/lib/posts';
-import { redirect } from 'next/navigation';
+import createPost from '@/actions/posts';
 
 export default function NewPostPage() {
-  
-  async function createPost(prevState, formData) {
-    'use server';
-    const title = formData.get('title');
-    const image = formData.get('image');
-    const content = formData.get('content');
-    let errors = [];
-
-    if (!title || title.trim().length === 0) {
-      errors.push('Title is required.');
-    }
-
-    if (!content || content.trim().length === 0) {
-      errors.push('Content is required.');
-    }
-
-    if (!image || image.size === 0) {
-      errors.push('An image must be attached.');
-    }
-
-    if (errors.length > 0) {
-      return { errors };
-    }
-
-    await storePost({
-      imageUrl: '',
-      title,
-      content,
-      userId: 1
-    });
-
-    redirect('/feed');
-  }
-
   return (
     <PostForm action={createPost}/>
   )
