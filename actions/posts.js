59d083463bb40acdb9b4b39819debed59528d50d
J-Skarbek <john@snarkmediaagency.com>
2024-05-10 14:10:49 -0500

    Add path revalidation as method to update liked symbol

--- actions/posts.js
+++ actions/posts.js
@@ -3,6 +3,7 @@
 import { redirect } from "next/navigation";
 import { storePost, updatePostLikeStatus } from "@/lib/posts";
 import { uploadImage } from "@/lib/cloudinary";
+import { revalidatePath } from "next/cache";
 
 export default async function createPost(prevState, formData) {
   const title = formData.get('title');
@@ -45,5 +46,6 @@ export default async function createPost(prevState, formData) {
 }
 
 export async function togglePostLikeStatus(postId) {
-  updatePostLikeStatus(postId, 2);
+  await updatePostLikeStatus(postId, 2);
+  revalidatePath('/feed');
 }
