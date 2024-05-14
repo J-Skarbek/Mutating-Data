f14fd1331d4fefb95e2e080c10db44cd83866cc9
J-Skarbek <john@snarkmediaagency.com>
2024-05-14 12:24:52 -0500

    Use Cloudinary features to resize and serve images

--- components/posts.js
+++ components/posts.js
@@ -6,6 +6,17 @@ import { togglePostLikeStatus } from '@/actions/posts';
 import { useOptimistic } from 'react';
 import Image from 'next/image';
 
+//Note: this functionality of the imageLoader/loader prop really only comes
+//into play when using a cloud/remote image hosting that offers on-demand image
+//resizing via url query parameters -- https://cloudinary.com/documentation/image_transformations
+function imageLoader(config) {
+  // console.log(config);
+  const urlStart = config.src.split('upload/')[0];
+  const urlEnd =  config.src.split('upload/')[1];
+  const transformations = `w_200,q_${config.quality}`;
+  return `${urlStart}upload/${transformations}/${urlEnd}`;
+}
+
 function Post({ post, action }) {
   return (
     <article className="post">
@@ -13,8 +24,15 @@ function Post({ post, action }) {
         <Image 
           src={post.image} 
           alt={post.title} 
-          fill
-
+          // fill 
+          // -- when using 'fill' you'll get a warning in the console
+          // if don't include a sizes prop while using a fill prop
+          // So, since cloudinary is serving images at 200 width, you can just
+          //set the width prop to 200 as well (or whatever your using for the width)
+          width={200}
+          //need to set the height with width in order to prevent errors when not
+          //using the fill prop, and then need to adjust the css accordingly
+          height={120}
           //Note, when you don't know the actual dimensions of user submitted
           //images, you don't want to overwrite them with width/height, but instead
           //use Fill. Fill will set the image css to 'position: absolute' which then
@@ -22,6 +40,12 @@ function Post({ post, action }) {
           // set the Image parent's componet to 'position: relative' in CSS
           // width={}
           // height={}
+
+          loader={imageLoader}
+          //the 'quality' prop takes a value between 0-100, behind the scenes
+          //nextJS will server/generate images with the approps quality value -- 
+          //This property works because the Image is invoking a loader prop/function
+          quality={80}
         />
         {/* <img src={post.image} alt={post.title} /> */}
       </div>
