c7d941fe5c10ff0d175cfdf500247bf96b4c854d
J-Skarbek <john@snarkmediaagency.com>
2024-05-14 14:24:34 -0500

    Explanation of layout.js metadata

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,10 @@ export const metadata = {
   description: 'Browse and share amazing posts.',
 };
 
+//The idea of posting metadata in a layout file is so it can be a fallback
+//for any pages w/o metadata, or, could also be seen as taking this info,
+//and merging it with any other metadata provided from any given page
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
